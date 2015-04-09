67773a36c000d6e426ac93fe8e4908ddff7ae764
zaqwer101 <platonovza@ya.ru>
2015-04-09 18:58:29 +0800

    V1.0

--- app/src/main/java/com/example/platonov/lesson2/MainActivity.java
+++ app/src/main/java/com/example/platonov/lesson2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.platonov.lesson2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,8 +9,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -17,6 +21,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,63 +29,71 @@ public class MainActivity extends ActionBarActivity {
     ImageButton imgBut;
     Button button_shop;
     TextView txtView, txtScoreClick;
-
-    public static long score;
-    public static long score_for_click;
+    public static LinearLayout f1;
+    public static long score = 0;
+    public static long score_for_click = 1;
+    Random rand = new Random();
     String str, string_score;
     String[] str_;
 
     //Методы
-    void writeFile(String filename, String str_) {
-        try {
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput(filename, MODE_PRIVATE)));
-            bw.write(str_);
+    void writeFile() {
+        try
+        {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput("cookie_score.txt", MODE_PRIVATE)));
+            bw.write(score + "-" + score_for_click);
             bw.close();
-        } catch (FileNotFoundException e) {
+        }
+
+        catch (FileNotFoundException e)
+        {
             e.printStackTrace();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+
+        {
             e.printStackTrace();
         }
     }
 
-    public String readFile(String FILENAME)
+    public void readFile()
     {
         try
         {
-            BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput(FILENAME)));
-            return br.readLine();
-        } catch (FileNotFoundException e)
-        {
-            return "0 1";
-        } catch (IOException e)
-        {
-            return "0 1";
+            BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput("cookie_score.txt")));
+            String x = br.readLine();
+            str_ = x.split("-");
+            this.score = Long.getLong(str_[0]);
+            this.score_for_click = Long.getLong(str_[1]);
+            Toast.makeText(this, score_for_click+"" , Toast.LENGTH_SHORT).show();
         }
-        catch (NullPointerException e)
+        catch (FileNotFoundException e)
+
         {
-            return "0 1";
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        try {
-            str_ = readFile("cookie_score").trim().split("\\s+");
-            score = Long.getLong(str_[0]);
-            score_for_click = Long.getLong(str_[1]);
-        }
-        catch(NullPointerException e)
-        {
-            score = 0;
-            score_for_click = 1;
-        }
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        readFile();
         button_shop = (Button) findViewById(R.id.button_shop);
         txtScoreClick = (TextView) findViewById(R.id.textView6);
         imgBut = (ImageButton) findViewById(R.id.imageButton);
         txtView = (TextView) findViewById(R.id.textView);
+        f1 = (LinearLayout) findViewById(R.id.linearlayot);
+        txtView.setText(score+"");
+        txtScoreClick.setText(score_for_click+"");
 
         // создаем единый обработчик нажатия
         View.OnClickListener oclBtn = new View.OnClickListener()
@@ -92,7 +105,10 @@ public class MainActivity extends ActionBarActivity {
                 {
                     case R.id.imageButton:
                     {
-                        txtScoreClick.setText(score_for_click+"");
+                        int x;
+                        f1.setBackgroundColor(x = -rand.nextInt(0xFFFFFF));
+                        imgBut.setBackgroundColor(x);
+                        txtScoreClick.setText(score_for_click + "");
                         score += score_for_click;
                         string_score = Long.toString(score);
                         txtView.setText(string_score);
@@ -113,26 +129,17 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        str = score+" "+score_for_click;
-        writeFile("cookie_score",str);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        menu.add("Сохранить");
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
+        writeFile();
+        Toast.makeText(this, "Сохранено", Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/platonov/lesson2/ShopActivity.java
+++ app/src/main/java/com/example/platonov/lesson2/ShopActivity.java
@@ -14,11 +14,21 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+/*
+    Для добавления новой постройки нужно добавить переменную кнопки, указать переменную стоимости и добавить в обработчик
+*/
 
 public class ShopActivity extends ActionBarActivity {
+    //Переменные кнопок
+    public Button btn_farm, btn_factory, btn_mine, btn_lab, btn_spsh;
 
-    public Button btn_farm, btn_factory, btn_mine, btn_lab;
     public TextView txtView;
+
+    public TextView txt1,txt2,txt3,txt4,txt5;
+
+    //Переменные стоимости построек
+    public static long farm_cost = 100,mine_cost = 500,factory_cost = 2000,lab_cost = 10000, spsh_cost = 100000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,12 +36,22 @@ public class ShopActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        //Присваивание кнопок
         btn_farm = (Button) findViewById(R.id.button);
         btn_factory = (Button) findViewById(R.id.button2);
         btn_mine = (Button) findViewById(R.id.button3);
         btn_lab = (Button) findViewById(R.id.button4);
+        btn_spsh = (Button) findViewById(R.id.button5);
         txtView = (TextView) findViewById(R.id.textView7);
 
+        txt1 = (TextView) findViewById(R.id.textView2);
+        txt2 = (TextView) findViewById(R.id.textView3);
+        txt3 = (TextView) findViewById(R.id.textView4);
+        txt4 = (TextView) findViewById(R.id.textView5);
+        txt5 = (TextView) findViewById(R.id.textView8);
+
+
+        // Обработчик
         View.OnClickListener oclBtn = new View.OnClickListener()
         {
             @Override
@@ -41,30 +61,36 @@ public class ShopActivity extends ActionBarActivity {
                 {
                     case R.id.button:
                     {
-                        if(MainActivity.score >= 100)
+                        if(MainActivity.score >= farm_cost)
                         {
                             MainActivity.score_for_click += 1;
-                            MainActivity.score -= 100;
+                            MainActivity.score -= farm_cost;
+                            farm_cost*=1.3;
+                            txt1.setText(farm_cost+"");
                             txtView.setText(MainActivity.score+"");
                         }
                         break;
                     }
                     case R.id.button2:
                     {
-                        if(MainActivity.score >= 500)
+                        if(MainActivity.score >= factory_cost)
                         {
-                            MainActivity.score_for_click += 6;
-                            MainActivity.score -= 500;
+                            MainActivity.score_for_click += 8;
+                            MainActivity.score -= factory_cost;
+                            factory_cost*=1.3;
+                            txt2.setText(factory_cost+"");
                             txtView.setText(MainActivity.score+"");
                         }
                         break;
                     }
                     case R.id.button3:
                     {
-                        if(MainActivity.score >= 2000)
+                        if(MainActivity.score >= mine_cost)
                         {
-                            MainActivity.score_for_click += 30;
-                            MainActivity.score -= 2000;
+                            MainActivity.score_for_click += 40;
+                            MainActivity.score -= mine_cost;
+                            mine_cost*=1.3;
+                            txt3.setText(mine_cost+"");
                             txtView.setText(MainActivity.score+"");
                         }
                         break;
@@ -72,10 +98,25 @@ public class ShopActivity extends ActionBarActivity {
 
                     case R.id.button4:
                     {
-                        if(MainActivity.score >= 10000)
+                        if(MainActivity.score >= lab_cost)
                         {
-                            MainActivity.score_for_click += 200;
-                            MainActivity.score -= 10000;
+                            MainActivity.score_for_click += 220;
+                            MainActivity.score -= lab_cost;
+                            lab_cost*=1.3;
+                            txt4.setText(lab_cost+"");
+                            txtView.setText(MainActivity.score+"");
+                        }
+                        break;
+                    }
+
+                    case R.id.button5:
+                    {
+                        if(MainActivity.score >= spsh_cost)
+                        {
+                            MainActivity.score_for_click += 600;
+                            MainActivity.score -= lab_cost;
+                            spsh_cost*=1.3;
+                            txt5.setText(spsh_cost+"");
                             txtView.setText(MainActivity.score+"");
                         }
                         break;
@@ -83,6 +124,8 @@ public class ShopActivity extends ActionBarActivity {
                 }
             }
         };
+
+        //Присвоение кнопкам обработчик
         btn_farm.setOnClickListener(oclBtn);
         btn_factory.setOnClickListener(oclBtn);
         btn_mine.setOnClickListener(oclBtn);
@@ -96,6 +139,7 @@ public class ShopActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_shop, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
